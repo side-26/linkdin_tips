476e1f3c211739f8dec48ad1cb1454d0729de75f
Side-26 <mr72302@gmail>
2024-02-29 21:18:11 +0330

    fix impliment initial-cache in useAppAsyncData

--- composables/useAppAsyncData.ts
+++ composables/useAppAsyncData.ts
@@ -1,21 +1,23 @@
 import type { AsyncData, AsyncDataOptions, NuxtError } from "nuxt/app";
 // @ts-ignore
 import type { AsyncDataExecuteOptions } from "nuxt/dist/app/composables/asyncData";
-import type { NuxtApp } from "nuxt/schema";
+import type { NuxtApp } from "~/node_modules/nuxt/dist/app/nuxt";
 export const useAppAsyncData = async <DataT, DataE>(
   key: string,
   handler: (ctx?: NuxtApp | undefined) => Promise<DataT>,
-  options?: AsyncDataOptions<DataT>
+  options?: AsyncDataOptions<DataT>,
+  initialCache:boolean=true
 ): Promise<AsyncData<DataT, Ref<DataE | null>>> => {
+  const nuxtApp = useNuxtApp();
   const { data, error, execute, pending, refresh, status } = useAsyncData<
     DataT,
     DataE
-    // @ts-ignore
   >(key, handler, {
+    getCachedData(key) {
+      return implimentInitialCache(key, nuxtApp, initialCache);
+    },
     ...options,
   });
-  // it just for handling errors in ssr-ssg mode
-  handleErrorPage(options?.server, error);
   return {
     data: data as Ref<DataT>,
     error: error as Ref<Ref<DataE | null> | null>,
@@ -25,14 +27,12 @@ export const useAppAsyncData = async <DataT, DataE>(
     status,
   };
 };
-function handleErrorPage(isServer: boolean | undefined, error: any) {
-  if (isServer === true) {
-    if (unref(error)?.["statusCode"] || unref(error)?.["statusMessage"]) {
-      showError({
-        statusMessage: unref(error)?.["statusMessage"],
 
-        statusCode: unref(error)?.["statusCode"],
-      });
-    }
-  }
+function implimentInitialCache(
+  key: string,
+  nuxtApp: NuxtApp,
+  initialCache: boolean = true
+) {
+  if (initialCache)
+    return nuxtApp?.payload?.data[key] || nuxtApp?.static?.data[key];
 }
