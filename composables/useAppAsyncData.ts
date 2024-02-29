7adce552d231466fefbc67d671a54740949a9c5e
Side-26 <mr72302@gmail>
2024-02-29 18:11:07 +0330

    fix add config error page

--- composables/useAppAsyncData.ts
+++ composables/useAppAsyncData.ts
@@ -1,25 +1,21 @@
-import type { AsyncData, AsyncDataOptions } from "nuxt/app";
+import type { AsyncData, AsyncDataOptions, NuxtError } from "nuxt/app";
 // @ts-ignore
 import type { AsyncDataExecuteOptions } from "nuxt/dist/app/composables/asyncData";
 import type { NuxtApp } from "nuxt/schema";
 export const useAppAsyncData = async <DataT, DataE>(
   key: string,
   handler: (ctx?: NuxtApp | undefined) => Promise<DataT>,
-  options?: AsyncDataOptions<DataT>,
-  initialCache: boolean = true
+  options?: AsyncDataOptions<DataT>
 ): Promise<AsyncData<DataT, Ref<DataE | null>>> => {
-  const nuxtApp = useNuxtApp();
   const { data, error, execute, pending, refresh, status } = useAsyncData<
     DataT,
     DataE
     // @ts-ignore
   >(key, handler, {
-    getCachedData(key) {
-      if (initialCache)
-        return nuxtApp.payload?.data[key] || nuxtApp?.static?.data[key];
-    },
     ...options,
   });
+  // it just for handling errors in ssr-ssg mode
+  handleErrorPage(options?.server, error);
   return {
     data: data as Ref<DataT>,
     error: error as Ref<Ref<DataE | null> | null>,
@@ -29,3 +25,14 @@ export const useAppAsyncData = async <DataT, DataE>(
     status,
   };
 };
+function handleErrorPage(isServer: boolean | undefined, error: any) {
+  if (isServer === true) {
+    if (unref(error)?.["statusCode"] || unref(error)?.["statusMessage"]) {
+      showError({
+        statusMessage: unref(error)?.["statusMessage"],
+
+        statusCode: unref(error)?.["statusCode"],
+      });
+    }
+  }
+}
